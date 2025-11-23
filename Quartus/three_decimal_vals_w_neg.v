a3d3256d386204660f1cb96603fce31c1a08fa52
Luke Macenski <57783708+lmacenski@users.noreply.github.com>
2025-11-23 17:17:42 -0500

    Add rest of 7 Segment Logic, added topmodule

---
+++ Quartus/three_decimal_vals_w_neg.v
@@ -0,0 +1,31 @@
+module three_decimal_vals_w_neg (
+    input signed [7:0] val,
+    output [6:0] seg7_dig0,
+    output [6:0] seg7_dig1,
+    output [6:0] seg7_dig2,
+    output [6:0] seg7_neg_sign
+);
+
+    reg [7:0] abs_value;
+    reg [3:0] digit0, digit1, digit2;
+    reg is_negative;
+
+    always @(*) begin
+        is_negative = val[7];
+        if (is_negative) begin
+            abs_value = -val;
+        end else begin
+            abs_value = val;
+        end
+
+        digit0 = abs_value % 10;
+        digit1 = (abs_value / 10) % 10;
+        digit2 = (abs_value / 100) % 10;
+    end
+
+    seven_segment u0 (.digit(digit0), .seg(seg7_dig0));
+    seven_segment u1 (.digit(digit1), .seg(seg7_dig1));
+    seven_segment u2 (.digit(digit2), .seg(seg7_dig2));
+    seven_segment_negative u3 (.is_neg(is_negative), .seg(seg7_neg_sign));
+
+endmodule
