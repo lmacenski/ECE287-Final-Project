b71e0aa053b01cbe515a2871fb5e5ff34d2e6ea0
Luke Macenski <57783708+lmacenski@users.noreply.github.com>
2025-12-05 11:02:20 -0500

    Started FSM for Game States

--- Quartus/topmodule.v
+++ Quartus/topmodule.v
@@ -1,3 +1,39 @@
-module topmodule();
+module topmodule(
+	input clk,
+	input rst,
+	input start_btn,
+	input select_btn,
+	input up_btn,
+	input down_btn,
+	input [3:0] lane_btn,
+	
+	output hsync,
+	output vsync,
+	output [3:0] vga_r,
+	output [3:0] vga_g,
+	output [3:0] vga_b
+);
+	
+	//FSM GAME STATES
+	localparam START = 3'd0;
+	localparam SELECT = 3'd1;
+	localparam GAME = 3'd2;
+	localparam GAME_OVER = 3'd3;
+	
+	//DEFINE STATE REGISTERS
+	reg[2:0] state;
+	reg[2:0] next;
+	
+	
+	always@(posedge clk or negedge rst) begin
+		if (rst)
+			state <= START;
+		else
+			state <= next;
+	end
+	
+	
+	
+	
 
 endmodule
---
+++ Quartus/topmodule.v.bak
@@ -0,0 +1,3 @@
+module topmodule();
+
+endmodule
